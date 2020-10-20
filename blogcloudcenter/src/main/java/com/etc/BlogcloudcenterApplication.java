b0da84318ae705b155ac79d1e177b29e8e60535d
yang19999 <1203970760@qq.com>
2020-10-20 11:04:51 +0800

    配置服务中心

--- blogcloudcenter/src/main/java/com/etc/BlogcloudcenterApplication.java
+++ blogcloudcenter/src/main/java/com/etc/BlogcloudcenterApplication.java
@@ -2,8 +2,10 @@ package com.etc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class BlogcloudcenterApplication {
 
     public static void main(String[] args) {
