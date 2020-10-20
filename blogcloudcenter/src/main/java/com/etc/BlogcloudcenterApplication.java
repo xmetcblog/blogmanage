6f0489cc8461da585c69e927d61f1e9052402a80
yang19999 <1203970760@qq.com>
2020-10-20 09:47:22 +0800

    Initial commit

---
+++ blogcloudcenter/src/main/java/com/etc/BlogcloudcenterApplication.java
@@ -0,0 +1,13 @@
+package com.etc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogcloudcenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogcloudcenterApplication.class, args);
+    }
+
+}
---
+++ blogusers/src/main/java/com/etc/BlogusersApplication.java
@@ -0,0 +1,13 @@
+package com.etc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogusersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogusersApplication.class, args);
+    }
+
+}
