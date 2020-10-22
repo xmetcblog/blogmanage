f6a63c1fb5d11f2c56ad93469397e8ea3013a10c
yang19999 <1203970760@qq.com>
2020-10-22 10:09:59 +0800

    配置Article

--- blogarticle/src/main/java/com/etc/BlogarticleApplication.java
+++ blogarticle/src/main/java/com/etc/BlogarticleApplication.java
@@ -2,14 +2,12 @@ package com.etc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class BlogusersApplication {
+public class BlogarticleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BlogusersApplication.class, args);
+        SpringApplication.run(BlogarticleApplication.class, args);
     }
 
 }
