86ae965568f0707d9165a4a2d71f3cb02c6879ed
yang1999 <1203970760@qq.com>
2020-10-24 15:11:52 +0800

    article Test依赖修改

--- blogarticle/src/test/java/com/etc/BlogarticleApplicationTests.java
+++ blogarticle/src/test/java/com/etc/BlogarticleApplicationTests.java
@@ -4,15 +4,12 @@ import com.etc.dao.mapper.ArticleDaoMapper;
 import com.etc.dao.ArticleDaoRepository;
 import com.etc.entity.Article;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 class BlogarticleApplicationTests {
 
     @Resource
