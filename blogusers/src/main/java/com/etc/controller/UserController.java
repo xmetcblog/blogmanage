4b4d534c8a15e44c634d9bb1333965901c0c8134
yang1999 <1203970760@qq.com>
2020-10-24 19:01:09 +0800

    login 测试

--- blogusers/src/main/java/com/etc/controller/UserController.java
+++ blogusers/src/main/java/com/etc/controller/UserController.java
@@ -3,23 +3,26 @@ package com.etc.controller;
 import com.etc.entity.User;
 import com.etc.service.UserService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
-@Controller
+@CrossOrigin("*")
+@RestController
 @RequestMapping("/login")
 public class UserController {
     @Resource
     UserService userService;
 
     @RequestMapping("/validate")
-    @ResponseBody
     public User validate(String username, String password){
         System.out.println("请求成功！！");
+        User login = userService.login(username, password);
+        System.out.println(username);
+        System.out.println(password);
+        System.out.println(login);
         return userService.login(username,password);
     }
 
 }
+
